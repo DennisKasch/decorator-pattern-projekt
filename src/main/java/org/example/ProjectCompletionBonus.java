673667e136afc24268ce821843653f8ff7bde044
AntonFlpk <a.anton-ent@web.de>
2025-04-18 13:40:27 +0200

    32.2: Green Test for ProjectCompletionBonus constructor

--- src/main/java/org/example/ProjectCompletionBonus.java
+++ src/main/java/org/example/ProjectCompletionBonus.java
@@ -1,5 +1,22 @@
 package org.example;
 
-public class ProjectCompletionBonus {
 
+public class ProjectCompletionBonus extends Bonus{
+
+    private int completedProjects;
+    public ProjectCompletionBonus(double bonus, int completedProjects)
+    {
+        super(50);
+        this.completedProjects = 20;
+    }
+
+    //Method was added for TDD purposes.
+    public int getCompletedProjects() {
+        return completedProjects;
+    }
+
+    @Override
+    public double calculateBonus() {
+        return 0;
+    }
 }
