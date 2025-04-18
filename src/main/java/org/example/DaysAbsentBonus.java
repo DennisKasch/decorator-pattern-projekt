3d18fb627742e50cafcbe9773d63a57f73efafa6
AntonFlpk <a.anton-ent@web.de>
2025-04-18 11:07:26 +0200

    25.1: Red Test for DaysAbsentBonus constructor

--- src/main/java/org/example/DaysAbsentBonus.java
+++ src/main/java/org/example/DaysAbsentBonus.java
@@ -1,5 +1,25 @@
 package org.example;
 
-public class DaysAbsentBonus {
+public class DaysAbsentBonus extends Bonus {
 
+    private int daysAbsent;
+    private int performance;
+    public DaysAbsentBonus(double bonus, int daysAbsent, int performance) {
+        super(0);
+    }
+
+    //Method was added for TDD purposes.
+    public int getDaysAbsent() {
+        return daysAbsent;
+    }
+
+    //Method was added for TDD purposes.
+    public int getPerformance() {
+        return performance;
+    }
+
+    @Override
+    public double bonusBerechnen() {
+        return 0;
+    }
 }
--- src/test/java/org/example/DaysAbsentBonusTest.java
+++ src/test/java/org/example/DaysAbsentBonusTest.java
@@ -1,7 +1,25 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 class DaysAbsentBonusTest {
+    private DaysAbsentBonus daysAbsentBonus;
+    @BeforeEach
+    void setUp()
+    {
+        daysAbsentBonus = new DaysAbsentBonus(100, 15, 20);
+    }
+
+    @Test
+    void testValidConstructor()
+    {
+        //use same values as in setUp
+        assertThat(daysAbsentBonus.getBonus()).isEqualTo(100);
+        assertThat(daysAbsentBonus.getDaysAbsent()).isEqualTo(15);
+        assertThat(daysAbsentBonus.getPerformance()).isEqualTo(20);
+    }
 
 }
