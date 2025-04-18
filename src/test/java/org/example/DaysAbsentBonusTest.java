45869135347d6aa27c80ac0cc3493e4d0c69c434
AntonFlpk <a.anton-ent@web.de>
2025-04-18 11:39:34 +0200

    26.1: Red Test for DaysAbsentBonus.calculateBonus -> LowDaysAbsent and LowPerformance

--- src/test/java/org/example/DaysAbsentBonusTest.java
+++ src/test/java/org/example/DaysAbsentBonusTest.java
@@ -7,19 +7,24 @@ import static org.assertj.core.api.Assertions.*;
 
 class DaysAbsentBonusTest {
     private DaysAbsentBonus daysAbsentBonus;
-    @BeforeEach
-    void setUp()
-    {
-        daysAbsentBonus = new DaysAbsentBonus(100, 15, 20);
-    }
 
     @Test
     void testValidConstructor()
     {
+        daysAbsentBonus = new DaysAbsentBonus(100, 15, 20);
+
         //use same values as in setUp
         assertThat(daysAbsentBonus.getBonus()).isEqualTo(100);
         assertThat(daysAbsentBonus.getDaysAbsent()).isEqualTo(15);
         assertThat(daysAbsentBonus.getPerformance()).isEqualTo(20);
+    }
+
+    @Test
+    void testCalculateBonus_LowDaysAbsent_LowPerformance()
+    {
+        daysAbsentBonus = new DaysAbsentBonus(100, 5, 3);
+        double expectedDaysAbsentBonus = 150;
+        assertThat(daysAbsentBonus.calculateBonus()).isEqualTo(expectedDaysAbsentBonus);
     }
 
 }
