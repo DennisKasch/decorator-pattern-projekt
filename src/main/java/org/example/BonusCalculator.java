c5c45b0c6f787fda08a32dd135ba2effc9cade2d
AntonFlpk <a.anton-ent@web.de>
2025-04-18 14:33:18 +0200

    39.1: Red Test for BonusCalculator constructor

--- src/main/java/org/example/BonusCalculator.java
+++ src/main/java/org/example/BonusCalculator.java
@@ -1,5 +1,24 @@
 package org.example;
 
 public class BonusCalculator {
+    private Employee employee;
+    private BonusService bonusService;
 
+    public BonusCalculator(Employee employee, BonusService bonusService)
+    {
+
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public BonusService getBonusService() {
+        return bonusService;
+    }
+
+    public double calculateBonus()
+    {
+        return 0;
+    }
 }
--- src/test/java/org/example/BonusCalculatorTest.java
+++ src/test/java/org/example/BonusCalculatorTest.java
@@ -1,7 +1,23 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.*;
 
 class BonusCalculatorTest {
 
+
+    @Test
+    void testValidConstructor()
+    {
+        Employee employee = mock(Employee.class);
+        BonusService bonusService = mock(BonusService.class);
+
+        BonusCalculator bonusCalculator = new BonusCalculator(employee, bonusService);
+
+        assertThat(bonusCalculator.getBonusService()).isEqualTo(bonusService);
+        assertThat(bonusCalculator.getEmployee()).isEqualTo(employee);
+    }
 }
