c1a35d354ae87851829caf631d52be583b543d67
alexandraksv <alexandra.kossilov0401@gmail.com>
2025-04-19 10:58:38 +0200

    35.2: GreenTest for BaseBonus return fixed Bonus

--- src/main/java/org/example/BaseBonus.java
+++ src/main/java/org/example/BaseBonus.java
@@ -1,6 +1,17 @@
 package org.example;
 
-public class BaseBonus
+public class BaseBonus extends Bonus
 {
+    private static final double BASE_AMOUNT = 1000.0;
 
+    public BaseBonus()
+    {
+        super(BASE_AMOUNT);
+    }
+
+    @Override
+    public double calculateBonus()
+    {
+        return bonus;
+    }
 }
