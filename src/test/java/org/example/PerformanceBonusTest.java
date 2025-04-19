682cfb4696e358603b8fe2f6dda6b2d8c3d18951
alexandraksv <alexandra.kossilov0401@gmail.com>
2025-04-19 11:13:06 +0200

    36.1: Red Test for performanceBonus factor 1.0

--- src/test/java/org/example/PerformanceBonusTest.java
+++ src/test/java/org/example/PerformanceBonusTest.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class PerformanceBonusTest {
+class PerformanceBonusTest
+{
+    @Test
+    void shouldApplyFactor1_0ForPerformanceBelow7()
+    {
+        Employee employee = new Employee("Max", "Mustermann", 3, 6, 5, 2, false);
+        Bonus baseBonus = new BaseBonus();
+        Bonus performanceBonus = new PerformanceBonus(baseBonus, employee);
 
+        double result = performanceBonus.calculateBonus();
+
+        assertThat(result).isEqualTo(1000.0);
+    }
 }
