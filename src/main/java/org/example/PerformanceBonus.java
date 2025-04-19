8e2c9759f2eec77dac8b5539d9ea17d567af5b82
alexandraksv <alexandra.kossilov0401@gmail.com>
2025-04-19 11:19:10 +0200

    36.2: Green Test for performanceBonus factor 1.0

--- src/main/java/org/example/PerformanceBonus.java
+++ src/main/java/org/example/PerformanceBonus.java
@@ -1,5 +1,31 @@
 package org.example;
 
-public class PerformanceBonus {
+public class PerformanceBonus extends Bonus
+{
+    private final Bonus decoratedBonus;
+    private final Employee employee;
 
+    public PerformanceBonus(Bonus decoratedBonus, Employee employee)
+    {
+        super(decoratedBonus.getBonus());
+        this.decoratedBonus = decoratedBonus;
+        this.employee = employee;
+    }
+
+    @Override
+    public double calculateBonus()
+    {
+        double base = decoratedBonus.calculateBonus();
+        int performance = employee.getPerformance();
+
+        double factor = 0;
+
+        if(performance <= 6)
+        {
+            factor += 1.0;
+        }
+
+        bonus = base * factor;
+        return  bonus;
+    }
 }
