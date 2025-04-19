1b85b6e2626dd693b447c9b1afa4ca0cd598de9d
alexandraksv <alexandra.kossilov0401@gmail.com>
2025-04-19 10:56:45 +0200

    35.1: Red Test for BaseBonus return fixed Bonus

--- src/test/java/org/example/BaseBonusTest.java
+++ src/test/java/org/example/BaseBonusTest.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class BaseBonusTest {
+class BaseBonusTest
+{
+    @Test
+    void shouldReturnFixedBaseBonus()
+    {
+        BaseBonus baseBonus = new BaseBonus();
+
+        double calculated = baseBonus.calculateBonus();
+
+        assertThat(calculated).isEqualTo(1000.0);
+        assertThat(baseBonus.getBonus()).isEqualTo(1000.0);
+    }
 
 }
