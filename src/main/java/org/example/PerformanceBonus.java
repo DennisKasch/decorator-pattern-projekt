9e22da4634d4b37e0e2d8040d8b5b5fe491f6e5b
alexandraksv <alexandra.kossilov0401@gmail.com>
2025-04-24 16:13:32 +0200

    46.2: Green Test for PerformanceBonus when Performance is low

--- src/main/java/org/example/PerformanceBonus.java
+++ src/main/java/org/example/PerformanceBonus.java
@@ -1,5 +1,17 @@
 package org.example;
 
-public class PerformanceBonus
+public class PerformanceBonus extends Bonus
 {
+    private final int performance;
+    public PerformanceBonus(double bonus, int performance)
+    {
+        super(bonus);
+        this.performance = performance;
+    }
+
+    @Override
+    public double calculateBonus()
+    {
+        return bonus * 0.8;
+    }
 }
