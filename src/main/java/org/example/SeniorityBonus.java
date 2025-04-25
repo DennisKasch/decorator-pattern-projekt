62f59cf5bce9e5cadf38bfe6b0f52937ce593b63
Dennis <dennis.kasch.28@gmail.com>
2025-04-25 09:12:17 +0200

    47.1: Add Red-Test for SeniorityBonus

--- src/main/java/org/example/SeniorityBonus.java
+++ src/main/java/org/example/SeniorityBonus.java
@@ -2,13 +2,21 @@ package org.example;
 
 public class SeniorityBonus extends Bonus {
 
-
     private int companyAffiliation;
 
     public SeniorityBonus(double bonus, int companyAffiliation)
     {
         super(0);
     }
+
+    public int getCompanyAffiliation() {
+        return companyAffiliation;
+    }
+
+    public void setCompanyAffiliation(int companyAffiliation) {
+        this.companyAffiliation = companyAffiliation;
+    }
+
     @Override
     public double calculateBonus() {
         return 0;
--- src/test/java/org/example/SeniorityBonusTest.java
+++ src/test/java/org/example/SeniorityBonusTest.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SeniorityBonusTest {
 
+    private SeniorityBonus seniorityBonus;
+    @Test
+    void testValidConstructor() {
+        seniorityBonus = new SeniorityBonus(20, 5);
+
+        assertThat(seniorityBonus.getBonus()).isEqualTo(20);
+        assertThat(seniorityBonus.getCompanyAffiliation()).isEqualTo(5);
+    }
 }
