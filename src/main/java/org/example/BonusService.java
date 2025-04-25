da8945a076251c258b15c524f5016c0240210c33
Dennis <dennis.kasch.28@gmail.com>
2025-04-25 12:53:36 +0200

    57.1: Add red-test for testValidConstructor for BonusService constructor

--- src/main/java/org/example/BonusService.java
+++ src/main/java/org/example/BonusService.java
@@ -1,12 +1,20 @@
 package org.example;
 
 public class BonusService {
-    private double maximumValue;
     private double minimumValue;
+    private double maximumValue;
 
-    public BonusService(double maximumValue, double minimumValue)
+    public BonusService(double minimumValue, double maximumValue)
     {
 
+    }
+
+    public double getMinimumValue() {
+        return minimumValue;
+    }
+
+    public double getMaximumValue() {
+        return maximumValue;
     }
 
     public double calculateCorrectBonus(double bonusSum)
--- src/test/java/org/example/BonusServiceTest.java
+++ src/test/java/org/example/BonusServiceTest.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BonusServiceTest {
+    private BonusService bonusService;
 
+    @Test
+    void testValidConstructor() {
+        bonusService = new BonusService(0, 100);
+
+        assertThat(bonusService.getMinimumValue()).isEqualTo(0);
+        assertThat(bonusService.getMaximumValue()).isEqualTo(100);
+    }
 }
